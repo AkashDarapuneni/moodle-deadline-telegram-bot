68d229f03771fb0cbb513c1d7761ab9fc2166bad
AkashDarapuneni <akash.darapuneni4113@gmail.com>
2026-08-15 22:23:47 +0530

    Refactor Tollywood alerts and improve alert system

--- main.py
+++ main.py
@@ -22,14 +22,74 @@ app = FastAPI()
 application = Application.builder().token(TOKEN).build()
 
 # ---------------------------------------------------------
-# TOLLYWOOD STEALTH ALERTS DICTIONARY
+# TOLLYWOOD STEALTH ALERTS DICTIONARY (50+ Random Dialogues)
 # ---------------------------------------------------------
 TFI_CRAZY_ALERTS = {
-    "24h": ["⏳ *\"Samayam ledu mithrama...\"*\n\n24 hours is less than you think. Open your laptop!\nhttps://media.tenor.com/gautamiputra-satakarni.gif", "⏳ *\"LMS ante flower anukunnava? FIRE-U!\"*\n\n24 hours left, thaggedhe le!\nhttps://media.tenor.com/pushpa-allu-arjun.gif"],
-    "6h": ["🪓 *\"Flute jinka mundu oodu... LMS server mundu kaadu!\"*\n\nStop playing games!\nhttps://media.tenor.com/balayya-legend.gif", "🪓 *\"Little hearts tintava ra thuppasi vedhava? Velli assignment rayi!\"*\nhttps://media.tenor.com/ms-narayana.gif"],
-    "2h": ["🚨 *\"Don't trouble the deadline! If you trouble the deadline, deadline troubles you!\"*\n\n2 hours left!\nhttps://media.tenor.com/balayya-dont-trouble.gif", "🚨 *\"YamaGola aypotundi ra... LMS lo submit chey 2 hours ey undi!\"*\nhttps://media.tenor.com/ntr-yamadonga.gif"],
-    "1h": ["💣 *\"Akhandaaaa!!! LMS server eppudaina padipovachu, submit it immediately!\"*\nhttps://media.tenor.com/akhanda-balayya.gif", "💣 *\"Arey, nenu matladedi vinapadatledaa!! Just 60 minutes left.\"*\n\nDrop everything and upload!\nhttps://media.tenor.com/temper-ntr.gif"],
-    "50m": ["💀 *\"Arey entra idhi... asalu em jarugutondi akkada! 50 mins ki submit nokkara!\"*\nhttps://media.tenor.com/brahmi-king.gif", "💀 *\"Last minute server crash chusava... mind block aypoddi! UPLOAD UPLOAD!\"*\nhttps://media.tenor.com/ntr-shock.gif"]
+    "24h": [
+        "⏳ *\"Samayam ledu mithrama...\"*\n\n24 hours is less than you think. Open your laptop!\nhttps://media.tenor.com/gautamiputra-satakarni.gif",
+        "⏳ *\"Nenochesa ani cheppu... 24 hours timeline start aindi ani cheppu!\"*\n\nStart writing!\nhttps://media.tenor.com/aravinda-sametha-ntr.gif",
+        "⏳ *\"LMS ante flower anukunnava? FIRE-U!\"*\n\n24 hours left, thaggedhe le!\nhttps://media.tenor.com/pushpa-allu-arjun.gif",
+        "⏳ *\"Boss is back... to remind you!\"*\n\nInka 24 hours undi, slow ga start chey.\nhttps://media.tenor.com/chiranjeevi-boss.gif",
+        "⏳ *\"Sound vinapadutundha? 24 hours countdown start aindi!\"*\n\nGet ready.\nhttps://media.tenor.com/rangasthalam-ram-charan.gif",
+        "⏳ *\"Emtra idhi? Inka start cheyaledha? Aascharyapothunnanu... aavedhana chendutunnanu.\"*\nhttps://media.tenor.com/brahmi-jalsa.gif",
+        "⏳ *\"Naakonchem thikka undi... time ki assignment cheste chala ishtam.\"*\nhttps://media.tenor.com/gabbar-singh-pk.gif",
+        "⏳ *\"Arey thuppasi yadhava! 24 hours nundi cheptunna... em peekutunnav ra?\"*\nhttps://media.tenor.com/brahmi-badshah.gif",
+        "⏳ *\"Narakadam start cheste... 24 hours lo motham assignment complete avvali!\"*\nhttps://media.tenor.com/boyapati-balayya.gif",
+        "⏳ *\"Thappu evaru chesina... internals cut!\"*\n\nRepu submission undi marchipoku.\nhttps://media.tenor.com/janatha-garage.gif",
+        "⏳ *\"Cheyyi chusava entha rough ga undo... inka rayakapothe laptop baddhalavtundi.\"*\nhttps://media.tenor.com/venu-madhav.gif"
+    ],
+    "6h": [
+        "🪓 *\"Flute jinka mundu oodu... LMS server mundu kaadu!\"*\n\nStop playing games!\nhttps://media.tenor.com/balayya-legend.gif",
+        "🪓 *\"Bhayapaddava? Bhayam... naaku teliyani kotha padham... kani neeku modhalavvali!\"*\n\n6 Hours left!\nhttps://media.tenor.com/ntr-temper.gif",
+        "🪓 *\"Party leda pushpa?\"*\n\nAssignment rayi firstu, tarvata party!\nhttps://media.tenor.com/pushpa-party.gif",
+        "🪓 *\"Mokke kada ani peekesthe... peeka kostha!\"*\n\nChinnade kada ani lite theeskunte zero padutundi.\nhttps://media.tenor.com/indra-chiru.gif",
+        "🪓 *\"Okkokkadini kadu sharekhan... 100 lines oke sari rayali nuvvu ippudu!\"*\nhttps://media.tenor.com/magadheera-rc.gif",
+        "🪓 *\"Little hearts tintava ra thuppasi vedhava? Velli assignment rayi!\"*\n\nJust 6 hours left.\nhttps://media.tenor.com/ms-narayana.gif",
+        "🪓 *\"Evadu kodithe dimma thirigi mind block aipothundo... adhe ZERO internals.\"*\n\n6 hours left!\nhttps://media.tenor.com/pokiri-mahesh.gif",
+        "🪓 *\"Choodappa siddappa... 6 hours lo upload chesey, lekapothe kashtam.\"*\nhttps://media.tenor.com/simha-balayya.gif",
+        "🪓 *\"Idiot... inka 6 hours pettukuni em chestunnav ra nannagaru?\"*\nhttps://media.tenor.com/ntr-rakhi.gif",
+        "🪓 *\"Don't fire the fire... start the work!\"*\n\n6 hours to go!\nhttps://media.tenor.com/raviteja-kick.gif",
+        "🪓 *\"Endukura inka late chestunnav? Blood udukuthundi naku!\"*\nhttps://media.tenor.com/brahmi-blood.gif"
+    ],
+    "2h": [
+        "🚨 *\"Don't trouble the deadline! If you trouble the deadline, deadline troubles you!\"*\n\n2 hours left!\nhttps://media.tenor.com/balayya-dont-trouble.gif",
+        "🚨 *\"YamaGola aypotundi ra... LMS lo submit chey 2 hours ey undi!\"*\nhttps://media.tenor.com/ntr-yamadonga.gif",
+        "🚨 *\"Assalu thaggedhe le... 2 hours lone raseyali. Rule idi!\"*\nhttps://media.tenor.com/pushpa-allu.gif",
+        "🚨 *\"Nenu eppudo vacha... Inka nee assignment upload kaledenti?\"*\n\n2 Hours left!\nhttps://media.tenor.com/chiranjeevi-khaidi.gif",
+        "🚨 *\"Arey nee gunde dabadaba kottukuntunda? 2 hours ey undi ra!\"*\nhttps://media.tenor.com/ram-charan-heart.gif",
+        "🚨 *\"Ekkadikelturooo... 2 hours ey vundi, ekkadiki vellakunda kurchoni rayandi!\"*\nhttps://media.tenor.com/brahmi-race-gurram.gif",
+        "🚨 *\"Nenu okkasari fix aythe... time tho sambandam ledu, kani ikkada 2 hours lo finish aypovali!\"*\nhttps://media.tenor.com/balayya-angry.gif",
+        "🚨 *\"Kathi vethukkovalsina avasaram ledu... LMS lo red mark chudu, time aypotundi!\"*\nhttps://media.tenor.com/ntr-kathi.gif",
+        "🚨 *\"Enduku sir... maku ee siksha... just 2 hours ey migilindi!\"*\nhttps://media.tenor.com/vennela-kishore.gif",
+        "🚨 *\"Cut out choosi konni konni nammeyali dude... LMS last minute lo crash avvadam kuda anthe.\"*\n\nFast ga rayi!\nhttps://media.tenor.com/salaar-prabhas.gif",
+        "🚨 *\"Naa valla kadu ra babu... meeru mee last minute submissions-u!\"*\nhttps://media.tenor.com/brahmi-frustation.gif"
+    ],
+    "1h": [
+        "💣 *\"Akhandaaaa!!! LMS server eppudaina padipovachu, submit it immediately!\"*\nhttps://media.tenor.com/akhanda-balayya.gif",
+        "💣 *\"Arey, nenu matladedi vinapadatledaa!! Just 60 minutes left.\"*\n\nDrop everything and upload!\nhttps://media.tenor.com/temper-ntr.gif",
+        "💣 *\"Nuvvu nanda aithe nenu badri badrinath... upload the pdf right now!\"*\nhttps://media.tenor.com/badrinath-aa.gif",
+        "💣 *\"Seat kaadu keda... ranku radu! 1 hour lo upload cheyakapothe zero fix!\"*\nhttps://media.tenor.com/chiru-indra.gif",
+        "💣 *\"1 hour lo leader avthava... zero avthava? Decide and hit submit!\"*\nhttps://media.tenor.com/rc-nayak.gif",
+        "💣 *\"Siggundali... 1 hour pettukoni inka rayaleda? Ninnu evadu kapadaledu!\"*\nhttps://media.tenor.com/brahmi-siggundali.gif",
+        "💣 *\"LMS server ki uresukune time daggara padindi... mundu aa file attach chey roi!\"*\nhttps://media.tenor.com/ali-comedy.gif",
+        "💣 *\"Bommali... ninnu vadala... 1 hour lo upload cheyali anthe!\"*\nhttps://media.tenor.com/balayya-shouting.gif",
+        "💣 *\"Nuvvu marava ra... 1 hour munde edusthav epudu! Start clicking submit!\"*\nhttps://media.tenor.com/ntr-crying.gif",
+        "💣 *\"Musalode kani mahanubhavudu... ee 1 hour lo LMS hand isthundi!\"*\nhttps://media.tenor.com/sunil-comedy.gif",
+        "💣 *\"Naaku koncham mentall undi... 1 hour munde upload cheseyamani cheppa!\"*\nhttps://media.tenor.com/venkatesh-angry.gif"
+    ],
+    "50m": [
+        "💀 *\"Arey entra idhi... asalu em jarugutondi akkada! 50 mins ki submit nokkara!\"*\nhttps://media.tenor.com/brahmi-king.gif",
+        "💀 *\"Last minute server crash chusava... mind block aypoddi! UPLOAD UPLOAD!\"*\nhttps://media.tenor.com/ntr-shock.gif",
+        "💀 *\"Bothiga neethulu... rulesu levu. 50 mins lone em rasavo adi pettey!\"*\nhttps://media.tenor.com/balayya-rules.gif",
+        "💀 *\"Nenu panikimala vadini... but last minute lo thelivi ga upload chestha!\"*\nhttps://media.tenor.com/arya2-aa.gif",
+        "💀 *\"Annayya... upload button nokkey annayya! Server down aithundi!\"*\nhttps://media.tenor.com/chiru-annayya.gif",
+        "💀 *\"50 mins lo submit cheyakapothe em poyindi? GPA pothundi!\"*\nhttps://media.tenor.com/rc-gpa.gif",
+        "💀 *\"Thippara meesam... click cheyra submit button... risk theeskovaddu!\"*\nhttps://media.tenor.com/ms-narayana-meesam.gif",
+        "💀 *\"Ayya, na valla kadu... submit the partial file now or get a ZERO!\"*\nhttps://media.tenor.com/ayya-meme.gif",
+        "💀 *\"Chari... em chestunnav ra... 50 minutes ey undi... potham, andaram potham!\"*\nhttps://media.tenor.com/ntr-chari.gif",
+        "💀 *\"Neeku time aypoyindi... inka 50 minutes ey undi, upload chey bey!\"*\nhttps://media.tenor.com/balayya-warning.gif",
+        "💀 *\"Pellam oorelithe pandaga... kani assignment time aypothe narakam! SUBMIT!\"*\nhttps://media.tenor.com/venu-madhav-comedy.gif"
+    ]
 }
 
 async def analyze_task_with_ai(title: str):
@@ -156,12 +216,14 @@ async def features(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "🧠 **2. AI Syllabus RAG:** Upload PDFs and Gemini will quiz you.\n"
         "🆘 **3. Anonymous Savior Mode:** Connects stuck students with those who finished the task.\n"
         "⚙️ **4. Alert Settings:** Customize your 24h/6h/2h Tollywood meme frequency.\n"
-        "🎯 **5. Lock-In (Focus):** Pomodoro state-tracking to force deep work.\n\n"
+        "🎯 **5. Lock-In (Focus):** Pomodoro state-tracking to force deep work.\n"
+        "🦇 **6. Night Owl Badges:** Secret unlockables for late-night grinds.\n"
+        "🔥 **7. AI Roast Mode:** Let Gemini creatively insult your overdue tasks.\n\n"
         "_Select a setting to configure:_"
     )
     keyboard = InlineKeyboardMarkup([
         [InlineKeyboardButton("⚙️ Alert Settings", callback_data="coming_soon"), InlineKeyboardButton("🌅 Morning Digest", callback_data="coming_soon")],
-        [InlineKeyboardButton("🆘 Savior Mode (Beta)", callback_data="coming_soon")]
+        [InlineKeyboardButton("🆘 Savior Mode (Beta)", callback_data="coming_soon"), InlineKeyboardButton("🔥 AI Roast", callback_data="coming_soon")]
     ])
     await update.message.reply_text(msg, parse_mode="Markdown", reply_markup=keyboard)
 
@@ -294,6 +356,7 @@ async def check_reminders():
             alert_key, d.sent_50m_alert = "50m", True
 
         if alert_key:
+            # THIS RANDOMLY PICKS 1 OUT OF THE 10+ OPTIONS
             quote = random.choice(TFI_CRAZY_ALERTS[alert_key])
             msg = (
                 f"{quote}\n\n"
